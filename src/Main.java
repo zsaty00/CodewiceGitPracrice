ff27ae308b63ca09888459f5c62aa78422f9e0d1
zsaty00 <satylgan.dev@gmail.com>
2023-10-29 20:57:41 -0500

    comment

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        String str = "I am teacher";
+        System.out.println(str);
     }
 }
---
+++ src/test/LoginPage.java
@@ -0,0 +1,12 @@
+package test;
+
+public class LoginPage {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is lon page");
+
+    }
+
+
+}
