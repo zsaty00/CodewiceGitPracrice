13c9f33f15f27c7ab82a5c3851c48575562235d2
zsaty00 <satylgan.dev@gmail.com>
2023-10-29 22:29:58 -0500

    my comment

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,15 @@ public class Main {
         System.out.println(ch);
 
 
+        boolean bool = true;
+        boolean bool1 = false;
+        System.out.println(bool1);
+        System.out.println(bool);
         String first_name = "Bill";
         String last_name = "Gates";
         System.out.println( first_name );
         System.out.println( last_name );
-        
+
 
     }
 }
